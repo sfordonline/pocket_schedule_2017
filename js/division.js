083d6dda6d8041bedd15bb0f65032e632526b045
STEPHEN FORD <sford@stephens-mbp.attlocal.net>
2017-05-08 01:25:58 -0500

    add divisions

---
+++ js/division.js
@@ -0,0 +1,124 @@
+$(document).ready(function () {
+    $('#loadCowboys').click(function (e) {
+		$('#Cowboys').show();
+        $("#Window").attr("src", "http://www.dallascowboys.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadGiants').click(function (e) {
+		$('#Giants').show();
+		$("#Window").attr("src", "http://www.giants.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadEagles').click(function (e) {
+		$('#Eagles').show();
+		$("#Window").attr("src", "http://www.philadelphiaeagles.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadRedskins').click(function (e) {
+		$('#Redskins').show();
+		$("#Window").attr("src", "http://www.redskins.com/");
+    });
+});
+
+$(document).ready(function () {
+    $('#loadBills').click(function (e) {
+		$('#Bills').show();
+        $("#Window").attr("src", "http://www.buffalobills.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadDolphins').click(function (e) {
+		$('#Dolphins').show();
+		$("#Window").attr("src", "http://www.miamidolphins.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadPatriots').click(function (e) {
+		$('#Patriots').show();
+		$("#Window").attr("src", "http://www.patriots.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadJets').click(function (e) {
+		$('#Redskins').show();
+		$("#Window").attr("src", "http://www.newyorkjets.com/");
+    });
+});
+
+$(document).ready(function () {
+    $('#loadRavens').click(function (e) {
+		$('#Ravens').show();
+        $("#Window").attr("src", "http://www.baltimoreravens.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadBengals').click(function (e) {
+		$('#Bengals').show();
+		$("#Window").attr("src", "http://www.bengals.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadBrowns').click(function (e) {
+		$('#Browns').show();
+		$("#Window").attr("src", "http://www.clevelandbrowns.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadSteelers').click(function (e) {
+		$('#Steelers').show();
+		$("#Window").attr("src", "http://www.steelers.com/");
+    });
+});
+
+$(document).ready(function () {
+    $('#loadTexans').click(function (e) {
+		$('#Texans').show();
+        $("#Window").attr("src", "http://www.houstontexans.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadColts').click(function (e) {
+		$('#Colts').show();
+		$("#Window").attr("src", "http://www.colts.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadJaguars').click(function (e) {
+		$('#Jaguars').show();
+		$("#Window").attr("src", "http://www.jaguars.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadTitans').click(function (e) {
+		$('#Titans').show();
+		$("#Window").attr("src", "http://www.titansonline.com/");
+    });
+});
+
+$(document).ready(function () {
+    $('#loadBroncos').click(function (e) {
+		$('#Broncos').show();
+        $("#Window").attr("src", "http://www.denverbroncos.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadChief').click(function (e) {
+		$('#Chiefs').show();
+		$("#Window").attr("src", "http://www.chiefs.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadChargers').click(function (e) {
+		$('#Chargers').show();
+		$("#Window").attr("src", "http://www.chargers.com/");
+    });
+});
+$(document).ready(function () {
+    $('#loadRaiders').click(function (e) {
+		$('#Raiders').show();
+		$("#Window").attr("src", "http://www.raiders.com/");
+    });
+});
